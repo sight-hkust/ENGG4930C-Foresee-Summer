5c31b8e153444a717dd3aa804b3295f25fab1642
Alan <hymanae@connect.ust.hk>
2020-06-13 22:08:22 +0800

    [NEW]Working on the registration 	The Name Validation is not completed, so are the password confirmation. Only few test cases are set. 	[x] create firebase account 	[x] basic UI 	[x] validation set

--- src/components/RegisterForm/Register.js
+++ src/components/RegisterForm/Register.js
@@ -1,16 +1,29 @@
 
 import React, { Component } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { PatientRegisterForm } from './RegisterForm/PatientRegisterForm';
 import RegisterOptions from './RegisterOptions';
+import { Styles } from '../../../Styles/styles';
+import AppColors from '../../../Styles/colors';
+import { RegisterForm } from './RegisterForm';
 
 const RegistrationStack = createStackNavigator();
 
 
 function Register() {
     return (
-        <RegistrationStack.Navigator>
-            <RegistrationStack.Screen name='Registration Form' component={PatientRegisterForm} initialParams={{}} />
+        <RegistrationStack.Navigator
+            initialRouteName="RegisterOptions"
+        >
+            <RegistrationStack.Screen
+                name='Registration Form'
+                component={RegisterForm}
+                options={{
+                    title: '建立帳戶',
+                    headerStyle: Styles.headerStyle,
+                    headerTintColor: AppColors.headerTintWhite,
+                    headerTitleStyle: Styles.headerTitleStyle,
+                }}
+                initialParams={{}} />
             <RegistrationStack.Screen name='Register Options' component={RegisterOptions} />
         </RegistrationStack.Navigator>
     )
