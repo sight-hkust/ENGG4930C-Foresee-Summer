729a0248193b3b413a7e0238c598a09a9d85a26d
Alan <hymanae@connect.ust.hk>
2020-07-06 13:22:21 +0800

    [ADD] chi and eng name validation case for temp use

--- src/components/Registration/Schema/SchemaPatient.js
+++ src/components/Registration/Schema/SchemaPatient.js
@@ -1,6 +1,63 @@
 import { object, string, number, bool } from "yup";
 
 export const SchemaPatient = object().shape({
+  firstName: string().when("selectedNameField", {
+    is: (val) => val === "chi",
+    then: string()
+      .required("請輸入有效姓名")
+      .test({
+        name: "chi_firstname_validation",
+        test: function (val) {
+          let validChineseNameFormat = /^[\u4E00-\u9FA5]{1,4}$/g;
+          if (
+            val === undefined ||
+            val === null ||
+            !validChineseNameFormat.test(val)
+          ) {
+            console.log(validChineseNameFormat.test(val));
+            return this.createError({
+              message: "請輸入有效姓名",
+              path: "lastName",
+            });
+          }
+          return true;
+        },
+      }),
+    otherwise: null,
+  }),
+  lastName: string().when("selectedNameField", {
+    is: (val) => val === "chi",
+    then: string()
+      .required("請輸入有效姓名")
+      .test({
+        name: "chi_firstname_validation",
+        test: function (val) {
+          let validChineseNameFormat = /^[\u4E00-\u9FA5]{1,4}$/g;
+          if (
+            val === undefined ||
+            val === null ||
+            !validChineseNameFormat.test(val)
+          ) {
+            return this.createError({
+              message: "請輸入有效姓名",
+              path: "lastName",
+            });
+          }
+          return true;
+        },
+      }),
+    otherwise: null,
+  }),
+  surName: string().when("selectedNameField", {
+    is: (val) => val === "eng",
+    then: string().required("Please enter valid name"),
+    otherwise: null,
+  }),
+  lastNameName: string().when("selectedNameField", {
+    is: (val) => val === "eng",
+    then: string().required("Please enter valid name"),
+    otherwise: null,
+  }),
   birthday: string().required("請輸入出生年份和月份"),
   email: string()
     .email("電郵地址無效，請以有效格式輸入電郵(例如：foresee@gmail.com)")
