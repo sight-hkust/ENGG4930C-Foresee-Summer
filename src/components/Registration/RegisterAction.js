6067ff574a741d70190d03b97df7b6b2cf4d70ca
Alan <hymanae@connect.ust.hk>
2020-07-14 13:05:35 +0800

    [ADD BACK] FAMILY ACCOUNT ON USER SIDE [x] Create child account [x] Navigation after account create [ ] Need to fix the navigation problem: goBack from registration form to profile [ ] Rerender the records on Overview after records added on record screen

--- src/components/Registration/RegisterAction.js
+++ src/components/Registration/RegisterAction.js
@@ -1,79 +1,118 @@
 import { auth, database } from "../../config/config";
 import moment from "moment";
 import * as firebase from "firebase";
+import { nanoid } from "nanoid/async/index.native";
 require("firebase/functions");
 
 const writeUserData = ({
   uid = null,
   values,
   isProfessional,
-  patientUid,
   registerPatient = false,
+  patientUid = null,
+  registerChild = false,
+  childUid = null,
 }) => {
-  if (registerPatient) {
-    database.ref("professionals/" + uid + "/patients/" + patientUid).set({
-      firstName: values.firstName || "",
-      lastName: values.lastName || "",
-      surName: values.surName || "",
-      givenName: values.givenName || "",
-      phone: values.tel_number
-        ? values.tel_country_code + values.tel_number
-        : "",
-      uid: patientUid,
-      inactive: true,
-    });
-    database.ref("users/" + patientUid).set({
-      uid: patientUid,
-      firstName: values.firstName,
-      lastName: values.lastName,
-      email: values.email,
-      birthday: moment(values.birthday).toJSON(),
-      job: values.job,
-      history: values.history,
-      disease: values.disease,
-      inactive: true,
-    });
-    if (!values.parentSelectionDisalbed && values.parent.uid) {
-      database
-        .ref("users/" + values.parent.uid + "/familyMembers/" + patientUid)
-        .set({
-          firstName: values.firstName,
-          lastName: values.lastName,
-          inactive: true,
-          uid: patientUid,
-        });
-    }
-  } else {
-    if (!isProfessional) {
-      database.ref("/users/" + uid).set({
-        uid: uid,
-        email: values.email,
-        phone: values.tel_country_code + values.tel_number,
-        firstName: values.firstName,
-        lastName: values.lastName,
-        birthday: moment(values.birthday).toJSON(),
-        records: {},
+  console.log(registerChild);
+  switch (true) {
+    case registerPatient:
+      database.ref("professionals/" + uid + "/patients/" + patientUid).set({
+        uid: patientUid,
+        inactive: true,
+        firstName: values.firstName || "",
+        lastName: values.lastName || "",
+        surName: values.surName || "",
+        givenName: values.givenName || "",
       });
-      database.ref("userInfo/" + uid).set({
-        uid: uid,
+      database.ref("users/" + patientUid).set({
+        uid: patientUid,
+        inactive: true,
         firstName: values.firstName,
         lastName: values.lastName,
         email: values.email,
-        phone: values.tel_country_code + values.tel_number,
         birthday: moment(values.birthday).toJSON(),
+        job: values.job,
+        history: values.history,
+        disease: values.disease,
+      });
+      if (!values.parentSelectionDisalbed && values.parent.uid) {
+        database
+          .ref("users/" + values.parent.uid + "/familyMembers/" + patientUid)
+          .set({
+            uid: patientUid,
+            inactive: true,
+            firstName: values.firstName,
+            lastName: values.lastName,
+          });
+      }
+      break;
+    case registerChild:
+      database.ref("users/" + uid + "/familyMembers/" + childUid).set({
+        uid: childUid,
+        inactive: true,
+        firstName: values.firstName || "",
+        lastName: values.lastName || "",
+        surName: values.surName || "",
+        givenName: values.givenName || "",
+      });
+      database.ref("users/" + childUid).set({
+        uid: childUid,
+        inactive: true,
+        firstName: values.firstName || "",
+        lastName: values.lastName || "",
+        surName: values.surName || "",
+        givenName: values.givenName || "",
+        birthday: moment(values.birthday).toJSON(),
+        job: values.job,
+        history: values.history,
+        disease: values.disease,
         allowedSearch: values.allowedSearch,
       });
-    } else {
-      database.ref("/professionals/" + uid).set({
-        uid: uid,
-        firstName: values.firstName,
-        lastName: values.lastName,
-        email: values.email,
-        phone: values.tel_country_code + values.tel_number,
-        role: values.role,
-      });
-    }
+      break;
+    default:
+      if (!isProfessional) {
+        database.ref("/users/" + uid).set({
+          uid: uid,
+          email: values.email,
+          phone: values.tel_country_code + values.tel_number,
+          firstName: values.firstName,
+          lastName: values.lastName,
+          birthday: moment(values.birthday).toJSON(),
+          records: {},
+        });
+        /* database.ref("userInfo/" + uid).set({
+          uid: uid,
+          firstName: values.firstName,
+          lastName: values.lastName,
+          email: values.email,
+          phone: values.tel_country_code + values.tel_number,
+          birthday: moment(values.birthday).toJSON(),
+          allowedSearch: values.allowedSearch,
+        }); */
+      } else {
+        database.ref("/professionals/" + uid).set({
+          uid: uid,
+          firstName: values.firstName,
+          lastName: values.lastName,
+          email: values.email,
+          phone: values.tel_country_code + values.tel_number,
+          role: values.role,
+        });
+      }
+      break;
   }
+};
+
+export const registerChildAccount = async ({
+  values,
+  registerChild,
+  returnOnComplete,
+}) => {
+  console.log(registerChild);
+  let childUid = await nanoid();
+  childUid = "ch-" + childUid;
+  writeUserData({ registerChild, values, childUid, uid: auth.currentUser.uid });
+  returnOnComplete();
 };
 
 export const registerPatientAccount = async ({
@@ -106,6 +145,7 @@ export const createAccount = ({
   navigation,
   isProfessional,
   setServerError,
+  returnOnComplete,
 }) => {
   if (isProfessional) {
     auth
@@ -117,6 +157,7 @@ export const createAccount = ({
         }
         navigation.navigate("Tutorial");
         writeUserData({ uid, values, navigation, isProfessional });
+        returnOnComplete();
       })
       .catch((error) => {
         setServerError(error);
@@ -131,6 +172,7 @@ export const createAccount = ({
         }
         navigation.navigate("Tutorial");
         writeUserData({ uid, values, navigation, isProfessional });
+        returnOnComplete();
       })
       .catch((error) => {
         setServerError(error);
---
+++ src/components/Registration/Schema/SchemaRegisterChild.js
@@ -0,0 +1,92 @@
+import { object, string, number } from "yup";
+export const SchemaRegisterChild = object().shape({
+  selectedNameFields: string().test({
+    name: "name_validation",
+    test: function (val) {
+      const {
+        firstName,
+        lastName,
+        surName,
+        givenName,
+        selectedNameFields,
+      } = this.parent;
+
+      if (selectedNameFields == "chi") {
+        if (!firstName && !lastName && !surName && !givenName) {
+          return this.createError({
+            message: "請輸入姓名",
+            path: "chineseNameError",
+          });
+        }
+        if (!firstName && lastName) {
+          return this.createError({
+            message: "請輸入有效姓名",
+            path: "chineseNameError",
+          });
+        }
+        if (!surName && givenName) {
+          return this.createError({
+            message: "Please enter a valid name",
+            path: "englishNameError",
+          });
+        }
+      } else {
+        if (!firstName && !lastName && !surName && !givenName) {
+          return this.createError({
+            message: "Please enter a valid name",
+            path: "englishNameError",
+          });
+        }
+        if (!firstName && lastName) {
+          return this.createError({
+            message: "請輸入有效姓名",
+            path: "chineseNameError",
+          });
+        }
+        if (!surName && givenName) {
+          return this.createError({
+            message: "Please enter a valid name",
+            path: "englishNameError",
+          });
+        }
+      }
+
+      let chineseValidationFormat = /^[\u4E00-\u9FA5]{1,4}$/;
+      let englishValidationFormat = /^[a-zA-Z][0-9a-zA-Z .,'-]*$/;
+      let chineseNameValidationError = null;
+      let englishNameValidationError = null;
+
+      if (firstName || lastName) {
+        let chineseValidationResult =
+          chineseValidationFormat.test(lastName) &&
+          chineseValidationFormat.test(firstName);
+        if (chineseValidationResult == false) {
+          chineseNameValidationError = "請輸入有效中文姓名";
+        }
+      }
+      if (surName || givenName) {
+        let englishValidationResult =
+          englishValidationFormat.test(surName) &&
+          englishValidationFormat.test(givenName);
+        if (englishValidationResult == false) {
+          englishNameValidationError = "Please enter a valid name";
+        }
+      }
+      if (chineseNameValidationError) {
+        return this.createError({
+          message: chineseNameValidationError,
+          path: "chineseNameError",
+        });
+      } else {
+        if (englishNameValidationError) {
+          return this.createError({
+            message: englishNameValidationError,
+            path: "englishNameError",
+          });
+        }
+      }
+      return true;
+    },
+  }),
+  birthday: string().required("請輸入病人出生日期"),
+});
