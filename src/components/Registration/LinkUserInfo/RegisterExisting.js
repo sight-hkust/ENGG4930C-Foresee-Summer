4c54461d483b5603a47efdfe43b5fa792acb5bb4
VCLH <lhvincentc@gmail.com>
2020-07-27 01:01:45 +0800

    [Update] Remove Redundant Logo.js

--- src/components/Registration/LinkUserInfo/RegisterExisting.js
+++ src/components/Registration/LinkUserInfo/RegisterExisting.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { ScrollView } from "react-native-gesture-handler";
-import Logo from "../../../../Utils/Logo";
-import { Text } from "react-native";
-import { LinearGradientBackground } from "../../../../Utils/LinearGradientBackground";
+import React from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
+import Logo from '../../Utils/Logo';
+import { Text } from 'react-native';
+import { LinearGradientBackground } from '../../../../Utils/LinearGradientBackground';
 
 export const RegisterExisting = ({ navigation, route }) => {
   const { uidFound } = route.params;
