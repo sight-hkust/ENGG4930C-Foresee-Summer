0e74c23e90c26b1a5a80dc48da4d76cc74ef98a0
VCLH <lhvincentc@gmail.com>
2020-07-27 01:59:33 +0800

    [Update] Splash Screen
    I added the bluegreengradient.png as future image background

--- Utils/LinearGradientBackground.js
+++ Utils/LinearGradientBackground.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { LinearGradient } from "expo-linear-gradient";
-import { SafeAreaView, View } from "react-native";
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView, View } from 'react-native';
 
 export const LinearGradientBackground = ({ children, ...props }) => {
   return (
     <View>
       <LinearGradient
-        colors={["#2D9CDB", "#48B3BA", "#0ED984"]}
+        colors={['#2D9CDB', '#48B3BA', '#0ED984']}
         start={[0, 0.3]}
         end={[1, 1]}
         locations={[0.25, 0.5, 1]}
         style={{
-          height: "100%",
+          height: '100%',
         }}
         {...props}
       >
