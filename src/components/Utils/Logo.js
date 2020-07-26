f2eea5cda179d4868fed45593f7221afc8872055
VCLH <lhvincentc@gmail.com>
2020-07-27 00:47:20 +0800

    [Minor] ForeSEE -> ForeSee

--- src/components/Utils/Logo.js
+++ src/components/Utils/Logo.js
@@ -1,24 +1,16 @@
-import React from "react";
-import icon from "../../../assets/images/icon.png";
-import { Image, View, Text } from "react-native";
-import { StyleSheet } from "react-native";
-import {
-  ScreenWidth,
-  ScreenHeight,
-  FontScale,
-} from "../../../constant/Constant";
+import React from 'react';
+import icon from '../../../assets/images/icon.png';
+import { Image, View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { ScreenWidth, ScreenHeight, FontScale } from '../../../constant/Constant';
 
 export const Logo = ({ style, iconStyle, textStyle, hideText = false }) => {
   return (
     <View style={[styles.logoContainer, style]}>
-      <Image
-        style={[styles.icon, iconStyle]}
-        source={icon}
-        resizeMode="contain"
-      />
+      <Image style={[styles.icon, iconStyle]} source={icon} resizeMode="contain" />
       {hideText ? null : (
-        <View style={{ flex: 4, justifyContent: "center" }}>
-          <Text style={[styles.logoText, textStyle]}>ForeSEE</Text>
+        <View style={{ flex: 4, justifyContent: 'center' }}>
+          <Text style={[styles.logoText, textStyle]}>ForeSee</Text>
         </View>
       )}
     </View>
@@ -27,20 +19,20 @@ export const Logo = ({ style, iconStyle, textStyle, hideText = false }) => {
 
 const styles = StyleSheet.create({
   logoContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
     height: ScreenHeight * 0.225,
     width: ScreenWidth * 0.55,
-    alignSelf: "center",
+    alignSelf: 'center',
   },
   logoText: {
-    color: "white",
-    fontWeight: "700",
+    color: 'white',
+    fontWeight: '700',
     fontSize: 20,
     left: ScreenWidth * 0.03,
   },
   icon: {
     flex: 3,
-    height: "100%",
+    height: '100%',
   },
 });
 
