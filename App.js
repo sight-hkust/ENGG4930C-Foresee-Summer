8893d7db773246350f061c769031177a984178d1
norangai <nyngaiaa@connect.ust.hk>
2020-08-01 16:05:44 +0800

    phone nav bar

--- App.js
+++ App.js
@@ -1,54 +1,54 @@
-import 'react-native-gesture-handler';
-import React, { useState, useEffect } from 'react';
-import { Image, Dimensions, TouchableOpacity, StyleSheet, Platform } from 'react-native';
-import { Icon } from 'react-native-elements';
+import "react-native-gesture-handler";
+import React, { useState, useEffect } from "react";
+import { Image, Dimensions, TouchableOpacity, StyleSheet, Platform } from "react-native";
+import { Icon } from "react-native-elements";
 
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import GetEducatedScreen from './src/components/Education/GetEducated';
-import DashboardScreen from './src/components/Education/EducationGame/DashboardScreen';
-import EyeExerciseScreen from './src/components/Education/EyeExercise';
-import ArticleDetailScreen from './src/components/Education/ArticleDetail';
-import PostQuestion from './src/components/AskAnExpert/PostQuestionScreen';
-import AskAnExpertMainScreen from './src/components/AskAnExpert/AskAnExpertMainScreen';
+import GetEducatedScreen from "./src/components/Education/GetEducated";
+import DashboardScreen from "./src/components/Education/EducationGame/DashboardScreen";
+import EyeExerciseScreen from "./src/components/Education/EyeExercise";
+import ArticleDetailScreen from "./src/components/Education/ArticleDetail";
+import PostQuestion from "./src/components/AskAnExpert/PostQuestionScreen";
+import AskAnExpertMainScreen from "./src/components/AskAnExpert/AskAnExpertMainScreen";
 
-import RecordsScreen from './Screens/RecordsScreen';
-import AddRecordScreen from './Screens/AddRecordScreen';
-import OverviewScreen from './Screens/OverviewScreen';
+import RecordsScreen from "./Screens/RecordsScreen";
+import AddRecordScreen from "./Screens/AddRecordScreen";
+import OverviewScreen from "./Screens/OverviewScreen";
 
-import { Login } from './src/components/Login/Login';
-import Profile from './src/components/Profile/Profile';
-import { QRCodeScannerScreen } from './src/components/QRCodeScannerScreen/QRCodeScannerScreen';
+import { Login } from "./src/components/Login/Login";
+import Profile from "./src/components/Profile/Profile";
+import { QRCodeScannerScreen } from "./src/components/QRCodeScannerScreen/QRCodeScannerScreen";
 
-import { auth } from './src/config/config';
+import { auth } from "./src/config/config";
 
-import ProfMainMenu from './src/components/Professional/ProfMainMenu';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './src/reducers';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import ProfMainMenu from "./src/components/Professional/ProfMainMenu";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./src/reducers";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
-import SettingScreen from './src/components/Setting/Setting';
-import PrivacyPolicy from './src/components/Policy/PrivacyPolicy';
-import TermsAndCondition from './src/components/Policy/TermsAndCondition';
+import SettingScreen from "./src/components/Setting/Setting";
+import PrivacyPolicy from "./src/components/Policy/PrivacyPolicy";
+import TermsAndCondition from "./src/components/Policy/TermsAndCondition";
 
-import TutorialScreen from './src/components/Tutorial/Tutorial';
-import QrCode from './src/components/Profile/QrCode';
-import { RegistrationForm } from './src/components/Registration/RegistrationForm/RegistrationForm';
-import { FamilyRouter } from './src/components/Family/FamilyRouter';
-import { RegisterNavigator } from './src/components/Registration/RegisterNavigator';
-import { SplashScreen } from './src/components/Splash/SplashScreen';
-import HeaderRightButton from './Utils/HeaderRightButton';
-import { RecordTutorial, AddRecordTutorial } from './src/components/Tutorial/MiniTutorial';
-import ProfPatientRecordView from './src/components/Professional/ProfPatientRecrodView/ProfPatientRecordView';
-import { BackButton } from './src/components/Utils/BackButton';
-import { EditUserInfo } from './src/components/Profile/EditUserInfo';
-import { Surface } from 'react-native-paper';
-import { View } from 'react-native-animatable';
-import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import TutorialScreen from "./src/components/Tutorial/Tutorial";
+import QrCode from "./src/components/Profile/QrCode";
+import { RegistrationForm } from "./src/components/Registration/RegistrationForm/RegistrationForm";
+import { FamilyRouter } from "./src/components/Family/FamilyRouter";
+import { RegisterNavigator } from "./src/components/Registration/RegisterNavigator";
+import { SplashScreen } from "./src/components/Splash/SplashScreen";
+import HeaderRightButton from "./Utils/HeaderRightButton";
+import { RecordTutorial, AddRecordTutorial } from "./src/components/Tutorial/MiniTutorial";
+import ProfPatientRecordView from "./src/components/Professional/ProfPatientRecrodView/ProfPatientRecordView";
+import { BackButton } from "./src/components/Utils/BackButton";
+import { EditUserInfo } from "./src/components/Profile/EditUserInfo";
+import { Surface } from "react-native-paper";
+import { View } from "react-native-animatable";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -61,8 +61,8 @@ function ArticleScreen({ navigation, route }) {
   }
   return (
     <Stack.Navigator screenOptions={headerConfig}>
-      <Stack.Screen name="GetEducatedScreen" component={GetEducatedScreen} options={{ title: '護眼秘笈' }} />
-      <Stack.Screen name="DashboardScreen" component={DashboardScreen} options={{ title: '成就統計' }} />
+      <Stack.Screen name="GetEducatedScreen" component={GetEducatedScreen} options={{ title: "護眼秘笈" }} />
+      <Stack.Screen name="DashboardScreen" component={DashboardScreen} options={{ title: "成就統計" }} />
       <Stack.Screen name="ArticleDetailScreen" component={ArticleDetailScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
@@ -75,7 +75,7 @@ function ExerciseScreen({ navigation, route }) {
         ...headerConfig,
       }}
     >
-      <Stack.Screen name="EyeExerciseScreen" component={EyeExerciseScreen} options={{ title: '護眼運動' }} />
+      <Stack.Screen name="EyeExerciseScreen" component={EyeExerciseScreen} options={{ title: "護眼運動" }} />
     </Stack.Navigator>
   );
 }
@@ -87,7 +87,7 @@ function HomeScreen({ navigation, route }) {
         name="OverViewScreen"
         component={OverviewScreen}
         options={{
-          title: '',
+          title: "",
           headerRight: () => <HeaderRightButton navigation={navigation} />,
         }}
       />
@@ -95,7 +95,7 @@ function HomeScreen({ navigation, route }) {
         name="RecordsScreen"
         component={RecordsScreen}
         options={{
-          title: '',
+          title: "",
           headerRight: () => <HeaderRightButton navigation={navigation} type="question" content={RecordTutorial} />,
         }}
       />
@@ -103,11 +103,11 @@ function HomeScreen({ navigation, route }) {
         name="AddRecordScreen"
         component={AddRecordScreen}
         options={{
-          title: '新增資料',
+          title: "新增資料",
           headerRight: () => <HeaderRightButton navigation={navigation} type="question" content={AddRecordTutorial} />,
         }}
       />
-      <Stack.Screen name="Family Router" component={FamilyRouter} options={{ title: '' }} />
+      <Stack.Screen name="Family Router" component={FamilyRouter} options={{ title: "" }} />
     </Stack.Navigator>
   );
 }
@@ -115,8 +115,8 @@ function HomeScreen({ navigation, route }) {
 function FaqScreen({ navigation, route }) {
   return (
     <Stack.Navigator screenOptions={headerConfig}>
-      <Stack.Screen name="AskAnExpertMainScreen" component={AskAnExpertMainScreen} options={{ title: '專家解答' }} />
-      <Stack.Screen name="PostQuestion" component={PostQuestion} options={{ title: '撰寫問題' }} />
+      <Stack.Screen name="AskAnExpertMainScreen" component={AskAnExpertMainScreen} options={{ title: "專家解答" }} />
+      <Stack.Screen name="PostQuestion" component={PostQuestion} options={{ title: "撰寫問題" }} />
     </Stack.Navigator>
   );
 }
@@ -124,7 +124,7 @@ function FaqScreen({ navigation, route }) {
 function ProfileScreen({ navigation, route }) {
   return (
     <Stack.Navigator screenOptions={headerConfig}>
-      <Stack.Screen name="ProfileTabMain" component={Profile} options={{ title: '我的檔案' }} />
+      <Stack.Screen name="ProfileTabMain" component={Profile} options={{ title: "我的檔案" }} />
       <Stack.Screen name="QrCode" component={QrCode} options={{ headerShown: false }} />
       <Stack.Screen
         name="Register"
@@ -134,11 +134,11 @@ function ProfileScreen({ navigation, route }) {
           headerLeft: () => (
             <BackButton
               onPress={() => {
-                navigation.navigate('ProfileTabMain');
+                navigation.navigate("ProfileTabMain");
               }}
             />
           ),
-          headerTintColor: 'white',
+          headerTintColor: "white",
           title: null,
         }}
       />
@@ -147,15 +147,15 @@ function ProfileScreen({ navigation, route }) {
         component={EditUserInfo}
         options={{
           headerTransparent: true,
-          title: '個人資料',
+          title: "個人資料",
           headerLeft: () => (
             <BackButton
               onPress={() => {
-                navigation.navigate('ProfileTabMain');
+                navigation.navigate("ProfileTabMain");
               }}
             />
           ),
-          headerTintColor: 'white',
+          headerTintColor: "white",
           title: null,
         }}
       />
@@ -166,10 +166,10 @@ function ProfileScreen({ navigation, route }) {
 function ProfessionalScreen({ navigation, route }) {
   return (
     <Stack.Navigator screenOptions={headerConfig}>
-      <Stack.Screen name="ProfMainMenu" component={ProfMainMenu} options={{ title: '病人名單' }} />
-      <Stack.Screen name="Patient Record" component={ProfPatientRecordView} options={{ title: '' }} />
-      <Stack.Screen name="AddRecordScreen" component={AddRecordScreen} options={{ title: '新增資料' }} />
-      <Stack.Screen name="SettingScreen" component={SettingScreen} options={{ title: '設定' }} />
+      <Stack.Screen name="ProfMainMenu" component={ProfMainMenu} options={{ title: "病人名單" }} />
+      <Stack.Screen name="Patient Record" component={ProfPatientRecordView} options={{ title: "" }} />
+      <Stack.Screen name="AddRecordScreen" component={AddRecordScreen} options={{ title: "新增資料" }} />
+      <Stack.Screen name="SettingScreen" component={SettingScreen} options={{ title: "設定" }} />
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
@@ -178,21 +178,21 @@ function ProfessionalScreen({ navigation, route }) {
 function Main({ route, navigation }) {
   return (
     <Tab.Navigator
-      initialRouteName={auth.currentUser && auth.currentUser.displayName == 'professional' ? 'ProfessionalScreen' : 'HomeScreen'}
+      initialRouteName={auth.currentUser && auth.currentUser.displayName == "professional" ? "ProfessionalScreen" : "HomeScreen"}
       tabBarOptions={{
         keyboardHidesTabBar: true,
         showLabel: false,
-        activeTintColor: '#003973',
-        inactiveTintColor: '#2D9CDB',
+        activeTintColor: "#003973",
+        inactiveTintColor: "#2D9CDB",
         style: {
-          position: 'absolute',
-          backgroundColor: 'transparent',
-          height: Platform.OS === 'ios' ? hp('9%') : hp('8%'),
+          position: "absolute",
+          backgroundColor: "transparent",
+          height: Platform.OS === "ios" ? hp("9%") : Dimensions.get("screen").height * 0.08,
           borderTopWidth: 0,
-          borderTopColor: 'transparent',
-          paddingHorizontal: auth.currentUser.displayName == 'professional' ? 80 : 30,
+          borderTopColor: "transparent",
+          paddingHorizontal: auth.currentUser.displayName == "professional" ? 80 : 30,
           elevation: 0,
-          shadowColor: '#BED8FF',
+          shadowColor: "#BED8FF",
           shadowOpacity: 0,
           shadowOffset: {
             height: 0,
@@ -201,7 +201,7 @@ function Main({ route, navigation }) {
         },
       }}
     >
-      {auth.currentUser.displayName == 'professional' ? (
+      {auth.currentUser.displayName == "professional" ? (
         <>
           <Tab.Screen
             name="Register"
@@ -215,7 +215,7 @@ function Main({ route, navigation }) {
             name="ProfessionalScreen"
             component={ProfessionalScreen}
             options={{
-              tabBarIcon: () => <Image source={require('./assets/images/Icon_solid.png')} style={{ ...styles.icon, ...{ width: 55, height: 55 } }} />,
+              tabBarIcon: () => <Image source={require("./assets/images/Icon_solid.png")} style={{ ...styles.icon, ...{ width: 55, height: 55 } }} />,
             }}
           />
           <Tab.Screen
@@ -233,10 +233,10 @@ function Main({ route, navigation }) {
             name="GetEducated"
             component={ArticleScreen}
             options={{
-              tabBarLabel: '護眼秘笈',
+              tabBarLabel: "護眼秘笈",
               tabBarIcon: () => (
-                <TouchableOpacity onPress={() => navigation.navigate('GetEducated')}>
-                  <Image source={require('./assets/images/Articles_dark.png')} style={styles.icon} />
+                <TouchableOpacity onPress={() => navigation.navigate("GetEducated")}>
+                  <Image source={require("./assets/images/Articles_dark.png")} style={styles.icon} />
                 </TouchableOpacity>
               ),
             }}
@@ -246,11 +246,11 @@ function Main({ route, navigation }) {
             name="ExerciseScreen"
             component={ExerciseScreen}
             options={{
-              tabBarLabel: '護眼運動',
+              tabBarLabel: "護眼運動",
               tabBarIcon: () => (
                 <>
-                  <TouchableOpacity onPress={() => navigation.navigate('ExerciseScreen')}>
-                    <Image source={require('./assets/images/Exercise_dark.png')} style={styles.icon} />
+                  <TouchableOpacity onPress={() => navigation.navigate("ExerciseScreen")}>
+                    <Image source={require("./assets/images/Exercise_dark.png")} style={styles.icon} />
                   </TouchableOpacity>
                 </>
               ),
@@ -261,10 +261,10 @@ function Main({ route, navigation }) {
             name="HomeScreen"
             component={HomeScreen}
             options={{
-              tabBarLabel: '主頁',
+              tabBarLabel: "主頁",
               tabBarIcon: () => (
-                <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
-                  <Image source={require('./assets/images/Icon_solid.png')} style={{ ...styles.icon, ...{ width: 50, height: 50 } }} />
+                <TouchableOpacity onPress={() => navigation.navigate("HomeScreen")}>
+                  <Image source={require("./assets/images/Icon_solid.png")} style={{ ...styles.icon, ...{ width: 50, height: 50 } }} />
                 </TouchableOpacity>
               ),
             }}
@@ -274,10 +274,10 @@ function Main({ route, navigation }) {
             name="FaqScreen"
             component={FaqScreen}
             options={{
-              tabBarLabel: '專家解答',
+              tabBarLabel: "專家解答",
               tabBarIcon: () => (
-                <TouchableOpacity onPress={() => navigation.navigate('FaqScreen')}>
-                  <Image source={require('./assets/images/Qna_dark.png')} style={styles.icon} />
+                <TouchableOpacity onPress={() => navigation.navigate("FaqScreen")}>
+                  <Image source={require("./assets/images/Qna_dark.png")} style={styles.icon} />
                 </TouchableOpacity>
               ),
             }}
@@ -287,10 +287,10 @@ function Main({ route, navigation }) {
             name="ProfileScreen"
             component={ProfileScreen}
             options={{
-              tabBarLabel: '個人檔案',
+              tabBarLabel: "個人檔案",
               tabBarIcon: () => (
-                <TouchableOpacity onPress={() => navigation.navigate('ProfileScreen')}>
-                  <Image source={require('./assets/images/Profile.png')} style={styles.icon} />
+                <TouchableOpacity onPress={() => navigation.navigate("ProfileScreen")}>
+                  <Image source={require("./assets/images/Profile.png")} style={styles.icon} />
                 </TouchableOpacity>
               ),
             }}
@@ -329,9 +329,9 @@ export default App = (props) => {
                 <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
                 <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
                 <Stack.Screen name="QR Scan" component={QRCodeScannerScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="Terms" component={TermsAndCondition} options={{ title: '條款及細則' }} />
-                <Stack.Screen name="Policy" component={PrivacyPolicy} options={{ title: '私隱政策' }} />
-                <Stack.Screen name="SettingScreen" component={SettingScreen} options={{ title: '設定' }} />
+                <Stack.Screen name="Terms" component={TermsAndCondition} options={{ title: "條款及細則" }} />
+                <Stack.Screen name="Policy" component={PrivacyPolicy} options={{ title: "私隱政策" }} />
+                <Stack.Screen name="SettingScreen" component={SettingScreen} options={{ title: "設定" }} />
               </>
             ) : (
               <>
@@ -351,23 +351,23 @@ export default App = (props) => {
 const headerConfig = {
   headerTransparent: true,
   headerTitleStyle: {
-    color: '#E1EDFF',
+    color: "#E1EDFF",
     fontSize: 25,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  headerTitleAlign: 'left',
+  headerTitleAlign: "left",
   headerBackTitleVisible: false,
   headerBackImage: () => <Icon name="md-arrow-back" type="ionicon" color="#E1EDFF" size={36} containerStyle={{ marginLeft: 20 }} />,
 };
 
 const styles = StyleSheet.create({
   icon: {
-    overflow: 'visible',
+    overflow: "visible",
     width: 35,
     height: 35,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: 'black',
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "black",
     shadowOpacity: 0.4,
     shadowOffset: {
       width: 4,
