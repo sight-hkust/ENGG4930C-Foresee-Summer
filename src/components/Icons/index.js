966a4dc50611dfbc940424fef477188b7e2c67ee
Alan <hymanae@connect.ust.hk>
2020-08-01 20:26:57 +0800

    [ADD] Gender Input and Gender Icon

---
+++ src/components/Icons/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Icon } from "react-native-elements";
+import { ScreenHeight } from "../../../constant/Constant";
+
+export const GenderIcon = () => (
+  <Icon
+    type="foundation"
+    name="male-female"
+    color={"white"}
+    size={ScreenHeight * 0.055}
+  />
+);
