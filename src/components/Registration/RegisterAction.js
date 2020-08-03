c4bbfd6e6670a6fdbff574c8c17182605aeb9ee5
VCLH <lhvincentc@gmail.com>
2020-08-03 22:22:08 +0800

    [Minor Change] Removing comments with "userInfo/"

--- src/components/Registration/RegisterAction.js
+++ src/components/Registration/RegisterAction.js
@@ -1,31 +1,23 @@
-import { auth, database } from "../../config/config";
-import moment from "moment";
-import * as firebase from "firebase";
-import { nanoid } from "nanoid/async/index.native";
-import { encryptData } from "../../utils/encryptData";
-require("firebase/functions");
+import { auth, database } from '../../config/config';
+import moment from 'moment';
+import * as firebase from 'firebase';
+import { nanoid } from 'nanoid/async/index.native';
+import { encryptData } from '../../utils/encryptData';
+require('firebase/functions');
 
-const writeUserData = ({
-  uid = null,
-  values,
-  isProfessional,
-  registerPatient = false,
-  patientUid = null,
-  registerChild = false,
-  childUid = null,
-}) => {
+const writeUserData = ({ uid = null, values, isProfessional, registerPatient = false, patientUid = null, registerChild = false, childUid = null }) => {
   console.log(registerChild);
   switch (true) {
     case registerPatient:
-      database.ref("professionals/" + uid + "/patients/" + patientUid).set({
+      database.ref('professionals/' + uid + '/patients/' + patientUid).set({
         uid: patientUid,
         inactive: true,
-        firstName: values.firstName || "",
-        lastName: values.lastName || "",
-        surName: values.surName || "",
-        givenName: values.givenName || "",
+        firstName: values.firstName || '',
+        lastName: values.lastName || '',
+        surName: values.surName || '',
+        givenName: values.givenName || '',
       });
-      database.ref("users/" + patientUid).set({
+      database.ref('users/' + patientUid).set({
         uid: patientUid,
         inactive: true,
         firstName: values.firstName,
@@ -37,34 +29,32 @@ const writeUserData = ({
         disease: values.disease,
       });
       if (!values.parentSelectionDisalbed && values.parent.uid) {
-        database
-          .ref("users/" + values.parent.uid + "/familyMembers/" + patientUid)
-          .set({
-            uid: patientUid,
-            inactive: true,
-            firstName: values.firstName,
-            lastName: values.lastName,
-            givenName: values.givenName,
-            surName: values.surName,
-          });
+        database.ref('users/' + values.parent.uid + '/familyMembers/' + patientUid).set({
+          uid: patientUid,
+          inactive: true,
+          firstName: values.firstName,
+          lastName: values.lastName,
+          givenName: values.givenName,
+          surName: values.surName,
+        });
       }
       break;
     case registerChild:
-      database.ref("users/" + uid + "/familyMembers/" + childUid).set({
+      database.ref('users/' + uid + '/familyMembers/' + childUid).set({
         uid: childUid,
         inactive: true,
-        firstName: values.firstName || "",
-        lastName: values.lastName || "",
-        surName: values.surName || "",
-        givenName: values.givenName || "",
+        firstName: values.firstName || '',
+        lastName: values.lastName || '',
+        surName: values.surName || '',
+        givenName: values.givenName || '',
       });
-      database.ref("users/" + childUid).set({
+      database.ref('users/' + childUid).set({
         uid: childUid,
         inactive: true,
-        firstName: values.firstName || "",
-        lastName: values.lastName || "",
-        surName: values.surName || "",
-        givenName: values.givenName || "",
+        firstName: values.firstName || '',
+        lastName: values.lastName || '',
+        surName: values.surName || '',
+        givenName: values.givenName || '',
         birthday: moment(values.birthday).toJSON(),
         job: values.job,
         history: values.history,
@@ -74,7 +64,7 @@ const writeUserData = ({
       break;
     default:
       if (!isProfessional) {
-        database.ref("/users/" + uid).set({
+        database.ref('/users/' + uid).set({
           uid: uid,
           email: values.email,
           tel_code: values.tel_country_code,
@@ -86,17 +76,8 @@ const writeUserData = ({
           birthday: moment(values.birthday).toJSON(),
           records: {},
         });
-        /* database.ref("userInfo/" + uid).set({
-          uid: uid,
-          firstName: values.firstName,
-          lastName: values.lastName,
-          email: values.email,
-          phone: values.tel_country_code + values.tel_number,
-          birthday: moment(values.birthday).toJSON(),
-          allowedSearch: values.allowedSearch,
-        }); */
       } else {
-        database.ref("/professionals/" + uid).set({
+        database.ref('/professionals/' + uid).set({
           uid: uid,
           firstName: values.firstName,
           lastName: values.lastName,
@@ -112,25 +93,15 @@ const writeUserData = ({
   }
 };
 
-export const registerChildAccount = async ({
-  values,
-  registerChild,
-  returnOnComplete,
-}) => {
+export const registerChildAccount = async ({ values, registerChild, returnOnComplete }) => {
   let childUid = await nanoid();
-  childUid = "ch-" + childUid;
+  childUid = 'ch-' + childUid;
   writeUserData({ registerChild, values, childUid, uid: auth.currentUser.uid });
   returnOnComplete();
 };
 
-export const registerPatientAccount = async ({
-  values,
-  registerPatient,
-  returnOnComplete,
-}) => {
-  let createPatientAccount = firebase
-    .functions()
-    .httpsCallable("createPatientAccount");
+export const registerPatientAccount = async ({ values, registerPatient, returnOnComplete }) => {
+  let createPatientAccount = firebase.functions().httpsCallable('createPatientAccount');
   createPatientAccount({
     email: values.email,
   })
@@ -148,22 +119,16 @@ export const registerPatientAccount = async ({
     });
 };
 
-export const createAccount = ({
-  values,
-  navigation,
-  isProfessional,
-  setServerError,
-  returnOnComplete,
-}) => {
+export const createAccount = ({ values, navigation, isProfessional, setServerError, returnOnComplete }) => {
   if (isProfessional) {
     auth
       .createUserWithEmailAndPassword(values.email, values.password)
       .then(function (userCreds) {
         const uid = userCreds.user.uid;
         if (userCreds) {
-          userCreds.user.updateProfile({ displayName: "professional" });
+          userCreds.user.updateProfile({ displayName: 'professional' });
         }
-        navigation.navigate("Tutorial");
+        navigation.navigate('Tutorial');
         writeUserData({ uid, values, navigation, isProfessional });
         returnOnComplete();
       })
@@ -176,9 +141,9 @@ export const createAccount = ({
       .then(function (userCreds) {
         const uid = userCreds.user.uid;
         if (userCreds) {
-          userCreds.user.updateProfile({ displayName: "user" });
+          userCreds.user.updateProfile({ displayName: 'user' });
         }
-        navigation.navigate("Tutorial");
+        navigation.navigate('Tutorial');
         writeUserData({ uid, values, navigation, isProfessional });
         returnOnComplete();
       })
