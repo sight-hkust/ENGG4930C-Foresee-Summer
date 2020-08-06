a99e2d22fe90b8af04944e57241b91f596ab0aaa
norangai <nyngaiaa@connect.ust.hk>
2020-08-06 15:27:32 +0800

    [delete]unnessary pic

--- src/components/Education/GetEducated.js
+++ src/components/Education/GetEducated.js
@@ -1,16 +1,12 @@
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, ScrollView, FlatList, Image, Dimensions } from 'react-native';
-import React, { Component } from 'react';
-import Expo from 'expo';
-import { database } from '../../config/config';
-import { LinearGradient } from 'expo-linear-gradient';
-import { ScreenWidth, ScreenHeight, FontScale } from '../../../constant/Constant';
-import HeaderRightButton from '../../../Utils/HeaderRightButton';
-import FABView from '../../../Utils/FAB';
-import MenuScreen from '../../../Utils/MenuScreen';
-import { actionCounter } from '../../utils/actionCounter';
-const thumbNail = require('../../../assets/images/interview.png');
-const eyeglasses = require('../../../assets/images/eyeglasses.jpg');
-const Setting = require('../../../assets/images/setting.png');
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, ScrollView, FlatList, Image, Dimensions } from "react-native";
+import React, { Component } from "react";
+import { database } from "../../config/config";
+import { LinearGradient } from "expo-linear-gradient";
+import { ScreenWidth, ScreenHeight } from "../../../constant/Constant";
+
+import FABView from "../../../Utils/FAB";
+import MenuScreen from "../../../Utils/MenuScreen";
+import { actionCounter } from "../../utils/actionCounter";
 
 function rand(x) {
   return Math.floor(Math.random() * x);
@@ -25,15 +21,15 @@ export default class GetEducated extends Component {
     super(props);
     this.state = {
       data: [],
-      topArticle: '',
+      topArticle: "",
     };
   }
 
   componentDidMount() {
     database
-      .ref('contents/articles')
-      .orderByChild('date')
-      .on('value', (snapshot) => {
+      .ref("contents/articles")
+      .orderByChild("date")
+      .on("value", (snapshot) => {
         var temp = [];
         snapshot.forEach((childSnapshot) => {
           var item = childSnapshot.val();
@@ -43,7 +39,7 @@ export default class GetEducated extends Component {
         });
         temp.reverse();
         let x = rand(temp.length);
-        console.log('topArticle', x, temp[x].subject);
+        console.log("topArticle", x, temp[x].subject);
         this.setState({ topArticle: temp[x] });
         this.setState({ data: temp });
       });
@@ -52,21 +48,21 @@ export default class GetEducated extends Component {
   render() {
     const pressHandler = () => {
       const id = this.state.topArticle._uid;
-      this.props.navigation.navigate('ArticleDetailScreen', { article_id: id });
-      actionCounter('articles', this.state.topArticle._uid, 'views');
+      this.props.navigation.navigate("ArticleDetailScreen", { article_id: id });
+      actionCounter("articles", this.state.topArticle._uid, "views");
     };
     return (
       <>
         <MenuScreen>
           <View
             style={{
-              backgroundColor: 'white',
-              height: '100%',
+              backgroundColor: "white",
+              height: "100%",
             }}
           >
             <View style={GetEducatedScreen.headerContainer}>
               <LinearGradient
-                colors={['#1872a7', '#5a74d1', '#a676ff']}
+                colors={["#1872a7", "#5a74d1", "#a676ff"]}
                 start={[0, 0.9]}
                 end={[1, 0.1]}
                 locations={[0, 0.5, 1]}
@@ -99,8 +95,8 @@ function Item({ item, navigation }) {
   //console.log(item.key);
   const id = item._uid;
   const pressHandler = () => {
-    navigation.navigate('ArticleDetailScreen', { article_id: id });
-    actionCounter('articles', item._uid, 'views');
+    navigation.navigate("ArticleDetailScreen", { article_id: id });
+    actionCounter("articles", item._uid, "views");
   };
   return (
     <TouchableOpacity onPress={pressHandler}>
@@ -120,22 +116,22 @@ const GetEducatedScreen = StyleSheet.create({
   header: {
     paddingTop: 40,
     marginHorizontal: 30,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   title: {
     fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
   headerContainer: {
-    overflow: 'hidden',
+    overflow: "hidden",
     borderBottomLeftRadius: 35,
     borderBottomRightRadius: 35,
-    position: 'absolute',
+    position: "absolute",
     height: 210,
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
   },
   topArticleContainer: {
     marginTop: 90,
@@ -146,7 +142,7 @@ const GetEducatedScreen = StyleSheet.create({
     flex: 1,
   },
   articleItem: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginVertical: 10,
   },
   itemImage: {
@@ -156,24 +152,24 @@ const GetEducatedScreen = StyleSheet.create({
     marginRight: 20,
   },
   articleSubject: {
-    flexWrap: 'wrap',
-    fontWeight: 'bold',
+    flexWrap: "wrap",
+    fontWeight: "bold",
     fontSize: 18,
-    color: '#24559E',
+    color: "#24559E",
   },
   articleDate: {
     fontSize: 12,
-    color: '#1772A6',
+    color: "#1772A6",
     paddingBottom: 5,
     paddingTop: 2,
   },
   articleAbstract: {
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
     fontSize: 14,
-    color: '#2D9CDB',
+    color: "#2D9CDB",
     paddingBottom: 10,
     borderBottomWidth: 1,
-    borderColor: '#24559E',
+    borderColor: "#24559E",
   },
   topArticleImage: {
     width: ScreenWidth - 60,
@@ -183,7 +179,7 @@ const GetEducatedScreen = StyleSheet.create({
     borderRadius: 14,
   },
   topArticleText: {
-    position: 'absolute',
+    position: "absolute",
     top: ScreenWidth * 0.6 - 86,
     paddingLeft: 20,
     paddingTop: 7,
@@ -193,8 +189,8 @@ const GetEducatedScreen = StyleSheet.create({
     borderBottomLeftRadius: 14,
     borderBottomRightRadius: 14,
     fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    fontWeight: "bold",
+    color: "white",
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
   },
 });
