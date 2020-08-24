40fd6e1bd5986cd80cdf2b7d990d9df8d896b394
norangai <nyngaiaa@connect.ust.hk>
2020-08-24 23:14:19 +0800

    [REMOVE]some console.log

--- Screens/RenderDescription.js
+++ Screens/RenderDescription.js
@@ -1,35 +1,35 @@
-import React, { Component, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { ScreenWidth, ScreenHeight } from '../constant/Constant';
+import React, { Component, useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { ScreenWidth, ScreenHeight } from "../constant/Constant";
 
 const severity = {
-  M: { light: 100, medium: 300, severe: 575, chi: '近視' },
-  H: { light: 0, medium: 200, severe: 500, chi: '遠視' },
-  A: { light: 0, medium: 75, severe: 175, chi: '散光' },
+  M: { light: 100, medium: 300, severe: 575, chi: "近視" },
+  H: { light: 0, medium: 200, severe: 500, chi: "遠視" },
+  A: { light: 0, medium: 75, severe: 175, chi: "散光" },
 };
 
 export const RenderDescription = (props) => {
   const { isLeft, ddlValue, data, selectedDate, index, dateArr } = props;
   if (data == null) return <Text style={DescriptionStyle.contentText}>暫無數據</Text>;
-  if (ddlValue == '3') return null;
+  if (ddlValue == "3") return null;
   let refractivechar, datatoshow;
   switch (ddlValue) {
-    case '0':
-      refractivechar = 'M';
+    case "0":
+      refractivechar = "M";
       datatoshow = isLeft ? data[selectedDate].L_Myopia : data[selectedDate].R_Myopia;
       break;
-    case '1':
-      refractivechar = 'H';
+    case "1":
+      refractivechar = "H";
       datatoshow = isLeft ? data[selectedDate].L_Hyperopia : data[selectedDate].R_Hyperopia;
       break;
-    case '2':
-      refractivechar = 'A';
+    case "2":
+      refractivechar = "A";
       datatoshow = isLeft ? data[selectedDate].L_CYL : data[selectedDate].R_CYL;
       break;
   }
   let sev = severity[refractivechar];
 
-  if (datatoshow != '0') {
+  if (datatoshow != "0") {
     return (
       <View>
         <Text style={DescriptionStyle.degreeText}>{datatoshow}度</Text>
@@ -50,7 +50,7 @@ export const RenderDescription = (props) => {
 export const RenderWarning = (props) => {
   const { degree, refractive } = props;
   const sev = severity[refractive];
-  console.log(sev);
+  //console.log(sev);
   if (degree < sev.light) {
     return (
       <View>
@@ -100,27 +100,27 @@ export const RenderIncreaseWarning = (props) => {
 
   const calDiff = (cur, prev) => {
     const diff = prev - cur;
-    if (diff > 0) return '淺了' + diff + '度';
-    else if (diff < 0) return '深了' + Math.abs(diff) + '度';
-    else return '度數不變';
+    if (diff > 0) return "淺了" + diff + "度";
+    else if (diff < 0) return "深了" + Math.abs(diff) + "度";
+    else return "度數不變";
   };
 
   if (isLeft) {
     switch (refractive) {
-      case '0': //myopia
+      case "0": //myopia
         return <Text style={DescriptionStyle.contentText}>對比上次紀錄: 近視{calDiff(curData.L_Myopia, prevData.L_Myopia)}</Text>;
-      case '1': //hyperopia
+      case "1": //hyperopia
         return <Text style={DescriptionStyle.contentText}>對比上次紀錄: 遠視{calDiff(curData.L_Hyperopia, prevData.L_Hyperopia)}</Text>;
-      case '2': //astigmatism
+      case "2": //astigmatism
         return <Text style={DescriptionStyle.contentText}>對比上次紀錄: 散光{calDiff(curData.L_CYL, prevData.L_CYL)}</Text>;
     }
   } else {
     switch (refractive) {
-      case '0': //myopia
+      case "0": //myopia
         return <Text style={DescriptionStyle.contentText}>對比上次紀錄: 近視{calDiff(curData.R_Myopia, prevData.R_Myopia)}</Text>;
-      case '1': //hyperopia
+      case "1": //hyperopia
         return <Text style={DescriptionStyle.contentText}>對比上次紀錄: 遠視{calDiff(curData.R_Hyperopia, prevData.R_Hyperopia)}</Text>;
-      case '2': //astigmatism
+      case "2": //astigmatism
         return <Text style={DescriptionStyle.contentText}>對比上次紀錄: 散光{calDiff(curData.R_CYL, prevData.R_CYL)}</Text>;
     }
   }
@@ -128,21 +128,21 @@ export const RenderIncreaseWarning = (props) => {
 
 const DescriptionStyle = StyleSheet.create({
   degreeText: {
-    color: '#2D9CDB',
+    color: "#2D9CDB",
     fontSize: ScreenWidth / 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
   contentText: {
-    textAlign: 'center',
-    color: '#2D9CDB',
+    textAlign: "center",
+    color: "#2D9CDB",
     fontSize: ScreenWidth / 25,
   },
   warningText: {
     marginLeft: 10,
     marginRight: 10,
     fontSize: 16,
-    color: '#FE7171',
-    textAlign: 'center',
+    color: "#FE7171",
+    textAlign: "center",
   },
 });
