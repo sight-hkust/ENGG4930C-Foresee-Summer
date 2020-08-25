69177c498933e17b0e3a2a879b79a735470e4a76
norangai <nyngaiaa@connect.ust.hk>
2020-08-25 16:40:54 +0800

    [Fixing]half product

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -20,7 +20,7 @@ const Profile = ({ navigation, route, userStore }) => {
   const { user } = userStore;
   const familyMembers = useSelector((state) => state.familyMembers);
   const [userData, setUserData] = useState(null);
-  console.log(user.uid);
+  console.log(user);
   const updateUserData = (uid) => {
     database
       .ref("users/" + uid)
