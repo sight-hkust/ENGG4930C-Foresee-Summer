1eb99dec10074d0d28a47c5afb53e7b385e0bb73
Dogu <dogudeniz.ugur@gmail.com>
2020-11-11 23:29:20 +0300

    Strings file structure changed

--- src/strings/Strings.js
+++ src/strings/Strings.js
@@ -1,14 +1,17 @@
 import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
 
+import en_strings from "./en";
+import zh_strings from "./zh";
+
 
 export function useLocalization() {
-
-    // Set the key-value pairs for the different languages you want to support.
     i18n.translations = {
-        en: { email: 'E-mail' },
-        zh: { email: '電子郵件' },
+        en: en_strings,
+        zh: zh_strings,
     };
-    // Set the locale once at the beginning of your app.
+
     i18n.locale = Localization.locale;
+    // When a value is missing from a language it'll fallback to another language with the key present.
+    i18n.fallbacks = true;
 }
---
+++ src/strings/zh.js
@@ -0,0 +1,13 @@
+let zh_strings = {
+    email: '電子郵件',
+    invalid_email: '電子郵件格式無效',
+    wrong_password: '密碼錯誤',
+    user_not_found: '帳號不存在，\n該帳戶有可能已被刪除',
+    password: '密碼',
+    login: '登入',
+    dont_have_an_account: '未有用戶? ',
+    register: '登記',
+    forget_password: '忘記密碼',
+};
+
+export default zh_strings;
